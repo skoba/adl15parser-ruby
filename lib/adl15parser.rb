4fedb88b029964c601566a76cffc1cd6a7c5c322
Shinji KOBAYASHI <skoba@moss.gr.jp>
2014-03-20 01:03:48 +0900

    adl-version 1.5

--- lib/adl15parser.rb
+++ lib/adl15parser.rb
@@ -1,9 +1,52 @@
 require 'openehr'
+require 'parslet'
 
 module OpenEHR
   module Parser
     class ADL15Parser < OpenEHR::Parser::Base
-      
+      def parse
+        transformer.apply(parslet_engine.parse(filestream.read))
+      end
+
+      def filestream
+        File.open(@filename, 'r:bom|utf-8')
+      end
+
+      def parslet_engine
+        ADL15Parslet.new
+      end
+
+      def transformer
+        ADL15Transformer.new
+      end
+    end
+
+    class ArchMock
+      attr_accessor :adl_version
+
+      def initialize(attribute = {})
+        @adl_version = attribute[:adl_version]
+      end
+    end
+
+    class ADL15Parslet < ::Parslet::Parser
+      root :archetype
+      rule(:archetype) do
+        sym_archetype >> space.maybe >>
+          (str('(') >> sym_adl_version >> sym_eq >> version_string.as(:adl_version) >> str(')')).maybe >>
+          any.repeat
+      end
+
+      rule(:sym_archetype) { str 'archetype'}
+      rule(:sym_adl_version) { str 'adl_version' }
+      rule(:version_string) { number >> str('.') >> number >> (str('.') >> number).maybe}
+      rule(:space) { match '\s' }
+      rule(:number) {match '[0-9]'}
+      rule(:sym_eq) { str '=' }
+    end
+
+    class ADL15Transformer < ::Parslet::Transform
+      rule(adl_version: simple(:s)) { ArchMock.new(adl_version: s.to_s) }
     end
   end
 end
--- spec/adl15parser_spec.rb
+++ spec/adl15parser_spec.rb
@@ -1,7 +1,16 @@
 describe OpenEHR::Parser::ADL15Parser do
-  let(:parser) { OpenEHR::Parser::ADL15Parser.new('examples/ADL15_tutorial/Archetypes/demographic/openEHR-DEMOGRAPHIC-ADDRESS.minimal.v1.adls') }
+  EXAMPLE = File.dirname(__FILE__) + '/examples/ADL15_tutorial/Archetypes/demographic/openEHR-DEMOGRAPHIC-ADDRESS.minimal.v1.adls'
+  let(:parser) { OpenEHR::Parser::ADL15Parser.new(EXAMPLE) }
 
   it 'should be an instance of ADL15Parser' do
     expect(parser).to be_an_instance_of OpenEHR::Parser::ADL15Parser
+  end
+
+  context 'parsed archetype instance' do
+    let(:archetype) { parser.parse }
+
+    it 'ADL version equals 1.5' do
+      expect(archetype.adl_version).to eq '1.5'
+    end
   end
 end
