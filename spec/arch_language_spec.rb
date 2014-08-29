eb62ba40cb5266fca3552adf2afca93cd92990ab
Shinji KOBAYASHI <skoba@moss.gr.jp>
2014-08-29 18:43:35 +0900

    focus on single attribute parsing

---
+++ spec/arch_language_spec.rb
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+describe 'arch_language parser' do
+  let(:parser) {
+    OpenEHR::Parser::ADL15Parslet.new.arch_language }
+
+  it 'parse language section' do
+    expect(parser).to parse ORIGINAL_LANGUAGE
+  end
+
+  it 'parse with multiple translations' do
+    expect(parser).to parse WITH_TRANSLATIONS
+  end
+  
+end
+
+
+ORIGINAL_LANGUAGE =<<DOC
+language
+	original_language = <[ISO_639-1::en]>
+DOC
+
+WITH_TRANSLATIONS =<<DOC
+	original_language = <[ISO_639-1::en]>
+	translations = <
+		["de"] = <
+			language = <[ISO_639-1::de]>
+			author = <
+				["name"] = <"Sebastian Garde, Jasmin Buck">
+				["organisation"] = <"Ocean Informatics, University of Heidelberg">
+			>
+		>
+		["zh-cn"] = <
+			language = <[ISO_639-1::zh-cn]>
+			author = <
+				["name"] = <"Chunlan Ma, Lin Zhang">
+				["organisation"] = <"Ocean Informatics, BIPH">
+				["email"] = <", linforest@163.com">
+			>
+			accreditation = <"What goes here?">
+		>
+		["ko"] = <
+			language = <[ISO_639-1::ko]>
+			author = <
+				["name"] = <"Seung-Jong Yu">
+				["organisation"] = <"NOUSCO Co.,Ltd.">
+				["email"] = <"seungjong.yu@gmail.com">
+			>
+		>
+		["ar-sy"] = <
+			language = <[ISO_639-1::ar-sy]>
+			author = <
+				["name"] = <"Mona Saleh">
+			>
+		>
+		["es-ar"] = <
+			language = <[ISO_639-1::es-ar]>
+			author = <
+				["name"] = <"Domingo Liotta">
+				["organisation"] = <"Universidad de Morón">
+				["email"] = <"domingo_liotta@hotmail.com">
+			>
+			accreditation = <"Universidad de Morón">
+		>
+		["fa"] = <
+			language = <[ISO_639-1::fa]>
+			author = <
+				["name"] = <"Shahla Foozonkhah">
+				["organisation"] = <"Ocean Informatics">
+				["email"] = <"shahla.foozonkhah@oceaninformatics.com">
+			>
+		>
+		["ru"] = <
+			language = <[ISO_639-1::ru]>
+			author = <
+				["name"] = <"Igor Lizunov">
+				["email"] = <"i.lizunov@infinnity.ru">
+			>
+		>
+		["ja"] = <
+			language = <[ISO_639-1::ja]>
+			author = <
+				["name"] = <"Shinji Kobayashi">
+				["organisation"] = <"Kyoto University">
+				["email"] = <"skoba@moss.gr.jp">
+			>
+		>
+		["nl"] = <
+			language = <[ISO_639-1::nl]>
+			author = <
+				["name"] = <"Marja Buur">
+				["organisation"] = <"M.C.A.">
+				["email"] = <"m.buur-krom@mca.nl">
+			>
+		>
+		["pt-br"] = <
+			language = <[ISO_639-1::pt-br]>
+			author = <
+				["name"] = <"Jussara Rözsch">
+				["organisation"] = <"OpenEHR  Foundation">
+				["email"] = <"jussara.macedo@gmail.com">
+			>
+			accreditation = <"Medical Doctor, Psychiarist, Clinical Modeller, openEHR Diretor, ehealth infostructuture WG ccoordinator- brazilian  ehealth program">
+		>
+	>
+DOC
--- spec/archetype_id_spec.rb
+++ spec/archetype_id_spec.rb
@@ -1,13 +1,18 @@
 describe 'archetype_id' do
   SIMPLE_ID = 'openEHR-DEMOGRAPHIC-ADDRESS.basic.v1'
   COMPLEX_ID = 'openehr-TEST_PKG-WHOLE.parent_with_uid_and_other_metadata.v1.0.0'
+  RC_ID = 'openEHR-EHR-OBSERVATION.no_ns_inherit_ns.v2.8.0-rc57'
   let(:parser) { OpenEHR::Parser::ADL15Parslet.new.archetype_id }
 
   it 'parses archetype_id' do
     expect(parser).to parse SIMPLE_ID
   end
 
-  it 'parsers complex_id' do
+  it 'parses complex_id' do
     expect(parser).to parse COMPLEX_ID
+  end
+
+  it 'parses id with rc' do
+    expect(parser).to parse RC_ID
   end
 end
